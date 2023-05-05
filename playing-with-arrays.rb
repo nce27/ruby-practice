12b5b9771fbd83d2c673117de1e0ef6e0916ba13
nce27 <nc3elliott@gmail.com>
2023-05-05 12:39:46 +0100

    messing around with array.join

---
+++ playing-with-arrays.rb
@@ -0,0 +1,14 @@
+# array creation
+arr1 = ["1", "2", "3", "4", "5"]
+
+# conversion to string with separators
+num_str = arr1.join("-")
+
+# outputs the array
+puts num_str
+
+arr2 = [1, 1, 5, 6, 8, 13, 2, 20]
+
+num_str2 = arr2.join("*,")
+
+puts num_str2
